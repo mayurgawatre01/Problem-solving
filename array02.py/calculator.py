48e0e68ca40be33a81b5b27ee6ae94b8715b3229
Mayur Gawatare <mayurgawatre38@gmail.com>
2026-08-16 21:32:10 +0530

    just practice

---
+++ array02.py/calculator.py
@@ -0,0 +1,22 @@
+def sum(a,b):
+    return a+b
+print(sum(1,2))
+
+def mult(a,b):
+    return a*b
+print(mult(3,4))    
+
+def div(a,b):
+    return a//b
+print(div(70,6)
+)
+
+def sub(a,b):
+    return a-b
+print(sub(5,2))
+
+
+print(sum(3,9999))
+
+
+print(mult(4,900))
